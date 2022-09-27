1f05928e7cbfcf223910c2ba98706b3f98b17281
suchkovdenis <ny1828@yandex.ru>
2022-09-27 17:22:27 +0600

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/SuchkovDenis/go-module-hello
+
+go 1.19
+
+require github.com/SuchkovDenis/go-module-greetings v0.0.0-20220927111802-ff32cfa6ce72
---
+++ hello.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	greetings "github.com/SuchkovDenis/go-module-greetings"
+	"log"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
+}
